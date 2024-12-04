2fa2037965cb6d89dd81bd9f7b1d7c2f42773d66
bolsolulaforever <fazedordecodigospicagrossa@gmail.com>
2024-12-04 19:28:45 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+//aqui vai um package que nao ta funcionando #paia
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main{
+	public static void main(String[] args) {
+		System.out.println("Você está andano no seguro bairro de canos matias, e você se depara, com um revolver em cima de uma mesa");
+		System.out.println("O que você faz?");
+		System.out.println("1-Roleta russa (yay)");
+		System.out.println("2-Adivinhar balas");
+		System.out.println("3-Tiro ao alvo com transeuntes inocentes");
+    // declaração de coisa
+	    Scanner scanner = new Scanner(System.in);
+	    int numero = scanner.nextInt();
+	//menu que seleciona entre arquivos    
+        switch(numero){
+            case 1:
+                
+            break; 
+            case 2:
+        
+            break;
+        
+        }
+	    
+	    
+	    
+	    
+	}
+}
---
+++ Roleta.java
@@ -0,0 +1,16 @@
+//package aqui
+import java.util.Scanner;
+import java.util.Random;
+public class Roleta{
+    public static void metodoRoleta(){
+    //declaração de variavel e etc declaração tlg
+        Scanner scanner = new Scanner(System.in);
+        int ba = scanner.nextInt();
+        boolean vida = true;
+        int municao []={0,0,0,0,0,0};
+    //começo da putaria    
+        System.out.println("Você pega o revolver");
+    //codigo aqui
+    
+    }
+}
